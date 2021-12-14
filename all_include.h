31aec7cc514e9393f7324ab84edda5f03549cff2
auyu <datealive102164@gmail.com>
2021-12-14 09:58:27 +0800

    Add new include header

---
+++ all_include.h
@@ -0,0 +1,16 @@
+#include "os2021_thread_api.h"
+#include "function_libary.h"
+#include "self_thread.h"
+#include "parsed_json.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <json-c/json.h>
+#include <ucontext.h>
+#include <stdlib.h>
+#include <stdio.h>
